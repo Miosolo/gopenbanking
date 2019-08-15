360c464ae0720e5f532d6bbec3bb6c4690e8edd8
better_git <576146819@qq.com>
2019-08-15 12:28:54 +0800

    <create> unit test and benchmark

--- chaincodeSecurity/chaincodeSecurity_test.go
+++ chaincodeSecurity/chaincodeSecurity_test.go
@@ -51,3 +51,39 @@ func TestChaincode(t *testing.T) {
 	res = stub.MockInvoke("1", [][]byte{[]byte("query"), []byte("out"), []byte("Songyue")})
 	fmt.Println("query Out result: ", string(res.Payload))
 }
+
+func BenchmarkCreateGetDelete(b *testing.B) {
+	cc := new(SimpleAsset)
+	stub := shim.NewMockStub("test", cc)
+
+	for i := 0; i < b.N; i++ {
+		stub.MockInvoke("1", [][]byte{[]byte("create"), []byte("Songyue"), []byte("0")})
+		stub.MockInvoke("1", [][]byte{[]byte("get"), []byte("Songyue")})
+		stub.MockInvoke("1", [][]byte{[]byte("delete"), []byte("Songyue")})
+	}
+}
+
+func BenchmarkCreateTransferQuery(b *testing.B) {
+	cc := new(SimpleAsset)
+	stub := shim.NewMockStub("test", cc)
+
+	for i := 0; i < b.N; i++ {
+		stub.MockInvoke("1", [][]byte{[]byte("create"), []byte("Songyue"), []byte("0")})
+		stub.MockInvoke("1", [][]byte{[]byte("create"), []byte("Yongmao"), []byte("100000")})
+		stub.MockInvoke("1", [][]byte{[]byte("transfer"), []byte("Yongmao"), []byte("Songyue"), []byte("1")})
+		stub.MockInvoke("1", [][]byte{[]byte("query"), []byte("out"), []byte("Yongmao")})
+		stub.MockInvoke("1", [][]byte{[]byte("query"), []byte("in"), []byte("Songyue")})
+	}
+}
+
+func BenchmarkCreateAddReduceDelete(b *testing.B) {
+	cc := new(SimpleAsset)
+	stub := shim.NewMockStub("test", cc)
+
+	for i := 0; i < b.N; i++ {
+		stub.MockInvoke("1", [][]byte{[]byte("create"), []byte("Songyue"), []byte("0")})
+		stub.MockInvoke("1", [][]byte{[]byte("add"), []byte("Songyue"), []byte("1")})
+		stub.MockInvoke("1", [][]byte{[]byte("reduce"), []byte("Songyue"), []byte("1")})
+		stub.MockInvoke("1", [][]byte{[]byte("delete"), []byte("Songyue")})
+	}
+}
