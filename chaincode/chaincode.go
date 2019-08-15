f0062c336a6299b00a8a2bea0d467d0d1be19364
better_git <576146819@qq.com>
2019-08-15 14:05:43 +0800

    <update> chaincode1.3.2

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -168,7 +168,17 @@ func add(stub shim.ChaincodeStubInterface, args []string) (string, error) {
 		return "", fmt.Errorf("Failed to set asset: %s with error: %s", args[0], err)
 	}
 
-	return fmt.Sprintf("Add is success! Account: %s; Remaining balance is: %d", args[0], intArgs1+intValueTemp), nil
+	nowValueTemp, err := stub.GetState(args[0])
+	intNowValueTemp, err := strconv.Atoi(string(nowValueTemp))
+	if err != nil {
+		return "", fmt.Errorf("Atoi fail! With Error: %s", err)
+	}
+
+	if (intValueTemp + intArgs1) == intNowValueTemp {
+		return fmt.Sprintf("Reduce is success! Account: %s; Remaining balance is: %d", args[0], intValueTemp-intArgs1), nil
+	} else {
+		return "", fmt.Errorf(fmt.Sprintf("Reduce failed! Error: database do not have correct number!"))
+	}
 }
 
 // args[0] represents account, args[1] represents money.
@@ -202,7 +212,18 @@ func reduce(stub shim.ChaincodeStubInterface, args []string) (string, error) {
 		return "", fmt.Errorf("Failed to set asset: %s;  With Error: %s", args[0], err)
 	}
 
-	return fmt.Sprintf("Reduce is success! Account: %s; Remaining balance is: %d", args[0], intValueTemp-intArgs1), nil
+	nowValueTemp, err := stub.GetState(args[0])
+	intNowValueTemp, err := strconv.Atoi(string(nowValueTemp))
+	if err != nil {
+		return "", fmt.Errorf("Atoi fail! With Error: %s", err)
+	}
+
+	if (intValueTemp - intArgs1) == intNowValueTemp {
+		return fmt.Sprintf("Reduce is success! Account: %s; Remaining balance is: %d", args[0], intValueTemp-intArgs1), nil
+	} else {
+		return "", fmt.Errorf(fmt.Sprintf("Reduce failed! Error: database do not have correct number!"))
+	}
+
 }
 
 // The function of this module is to create an account of ledger
@@ -262,7 +283,7 @@ func transfer(stub shim.ChaincodeStubInterface, args []string) (string, error) {
 	argsD[1] = args[2]
 	_, err := reduce(stub, argsD)
 	if err != nil {
-		return "", fmt.Errorf(fmt.Sprintf("Reduce debit account failed!"))
+		return "", fmt.Errorf(fmt.Sprintf("Reduce debit account failed! With error: %s", err))
 	}
 
 	//add money to the cebit account.
@@ -271,7 +292,7 @@ func transfer(stub shim.ChaincodeStubInterface, args []string) (string, error) {
 	argsC[1] = args[2]
 	_, err = add(stub, argsC)
 	if err != nil {
-		return "", fmt.Errorf(fmt.Sprintf("Add cebit account failed!"))
+		return "", fmt.Errorf(fmt.Sprintf("Add cebit account failed! With error: %s", err))
 	}
 	// store the transfer record into the database
 	// "out" means the money go out from one's account,
